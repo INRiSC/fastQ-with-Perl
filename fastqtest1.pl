22884be7a4c5c93eeb5aede85baff6ead4da22e6
INRiSC <zhujunda0@gmail.com>
2017-06-10 16:05:06 +0800

    Add files via upload

---
+++ fastqtest1.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl-w
+
+use strict;
+
+open FILE, '../jiankuihe-exome-1.fq' || die "Can not open"; #打开文件
+
+open OUT1,'>basequality.txt';
+open OUT2,'>score.txt';
+open OUT3,'>GC.txt';
+open OUT4,'>duplication.txt';
+
+$seq = 0;
+$qual = 0;
+$nl = 0;
+$readnum = 0;
+
+while (<FILE>) {
+        chomp;
+        $nl++;
+        $readnum++;
+      
+        if ($nl == 2) {
+                $seq = $_;
+                if(exists $hashsq{$seq}) {
+                        $hashsq{$seq}++;
+                }
+                else{
+                        $hashsq{$seq} == 1;
+                }
+                $G = $seq =~ tr/G/G/;
+                $C = $seq =~ tr/C/C/;
+                $gc = int(100*($G + $C)/(length $seq));
+                if(exists $hashgc{$gc}) {
+                        $hashgc{$gc}++;
+                }
+                else{
+                        $hashgc{$gc} == 1;
+                }
+        }
+        elsif ($nl == 4) {
+                $qual = $_;
+                $count = 1;
+                while($count <= (length $qual)) {
+                        $p[$count] += 10**((-1)*(ord($qual[$count])-64));
+                        $psc = 0;
+                        $psc += 10**((-1)*(ord($qual[$count])-64));
+                        $count++;
+                }
+                $sc = 0;
+                $sc = int(-log($psc));
+                if(exists $hashsc{$sc}) {
+                        $hashsc{$sc}++;
+                }
+                else{
+                        $hashsc{$sc} == 1;
+                }
+                $nl = 0;
+        }
+}
+
+$count1 = 0;
+$count2 = 0;
+$Q = 0;
+for($count1 < $readnum){
+        for($count2 < 101){
+                $Q[$count2] = int(-log($p[$count2]));
+        }
+}
+for ($i=1; $i<=100; $i++){
+        print OUT1 "$i $Q[i]/$readnum\n";
+}
+
+foreach $key (keys %hashsc) {
+        $value = $hashsc{$key};
+        print OUT2 "$key $value\n";
+}
+
+foreach $key (keys %hashgc) {
+    $value = $hashgc{$key};
+    print OUT3 "$key $value\n";
+}
+
+i = 0;
+foreach (sort keys %hashsq) {
+    $value = $hashsq{$key};
+    for ($i=1; $i<=10; $i++){
+        print OUT4 "$i $value\n";
+        }
+}
